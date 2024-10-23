884d228caad88729d767892d29d057511b4f70fa
Darian Samuel Hood <hoodds@proton.me>
2024-10-23 13:57:50 -0500

    Finished a TODO and started working on another
    Also added the base file system layout for the drive.
    'Read and automaticly run ps1 files in .\scripts' TODO is done.
    Activly working on 'Update and/or Change Environment Variables'.

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,33 @@
 #TODO: Update and/or Change Environment Variables
 #TODO: Edit and Change Modules path to .\modules
-#TODO: Read and automaticly run ps1 files in .\scripts
+#///TODO: Read and automaticly run ps1 files in .\scripts
+
+<# File System Layout ╠ ║ ╔ ╚
+.\
+╠ .cache | cache files
+╠ .conf  | config files
+╠ .libs  | library files
+╠ sbin   | program lanagues
+╠ srv    | programs
+╚ proj   | projects
+#>
+
+using namespace System
+
+$local:Drive = (Get-Volume -FileSystemLabel 'Drive ( Remote )')
+$RemoteDriveLetter = $local:Drive.DriveLetter
+$RemoteDrive = "$RemoteDriveLetter" + ':'
+$local:RemoteDriveRootFolders = @{
+    'CACH' = "$RemoteDrive\.cache"
+    'CONF' = "$RemoteDrive\.conf"
+    'LIBS' = "$RemoteDrive\.libs"
+    'SBIN' = "$RemoteDrive\sbin"
+    'SRV'  = "$RemoteDrive\srv"
+    'PROJ' = "$RemoteDrive\proj"
+}
+
+
+Write-Host $RemoteDriveLetter
+Write-Host $RemoteDrive
+
+Get-ChildItem .\scripts | ForEach-Object -Process { & $_ }
