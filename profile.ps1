2294e266b0800ff4df021b2f3f3a47f616300691
Darian Samuel Hood <hoodds@proton.me>
2024-10-24 13:34:23 -0500

    Finished TODOs and Creates some more.
    Finished TODOs 'Edit and Change Modules path to
    .\.conf\powershell\modules' and 'Update and/or Change Environment
    Variables. Create new TODO 'Create Alias'.

--- profile.ps1
+++ profile.ps1
@@ -1,16 +1,7 @@
-#TODO: Update and/or Change Environment Variables
-#TODO: Edit and Change Modules path to .\modules
+#///TODO: Update and/or Change Environment Variables
+#///TODO: Edit and Change Modules path to .\modules
 #///TODO: Read and automaticly run ps1 files in .\scripts
-
-<# File System Layout ╠ ║ ╔ ╚
-.\
-╠ .cache | cache files
-╠ .conf  | config files
-╠ .libs  | library files
-╠ sbin   | program lanagues
-╠ srv    | programs
-╚ proj   | projects
-#>
+#TODO: Create Alias
 
 using namespace System
 
@@ -25,9 +16,53 @@ $local:RemoteDriveRootFolders = @{
     'SRV'  = "$RemoteDrive\srv"
     'PROJ' = "$RemoteDrive\proj"
 }
+$local:Path = ($local:RemoteDriveRootFolders['SBIN'] + ';') `
+    + ([String](Get-ChildItem $local:RemoteDriveRootFolders['SBIN'] | ForEach-Object -Process { 
+            if ( $_ -match 'git' ) { return '' }
+            if ( $_ -match 'starship' ) { return '' }
+            else { return $_.FullName + ';' } 
+        }) -replace ' ', '') `
+    + ($local:RemoteDriveRootFolders['SBIN'] + '\git\bin;') `
+    + ($local:RemoteDriveRootFolders['SRV'] + '\vscode\bin;')
 
 
-Write-Host $RemoteDriveLetter
-Write-Host $RemoteDrive
+if ( [Environment]::GetEnvironmentVariable('RemoteDriveLetter', [EnvironmentVariableTarget]::User) -notmatch $RemoteDriveLetter ) 
+{ [Environment]::SetEnvironmentVariable('RemoteDriveLetter', $RemoteDriveLetter, [EnvironmentVariableTarget]::User) }
 
+if ( [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:Path) ) 
+{ [Environment]::SetEnvironmentVariable('Path', ( $local:Path + [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) ), [EnvironmentVariableTarget]::User ) }
+
+if ( [Environment]::GetEnvironmentVariable('PSModulePath', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['CONF'] + '\powershell\modules'))
+{ [Environment]::SetEnvironmentVariable('PSModulePath', $local:RemoteDriveRootFolders['CONF'] + '\powershell\modules', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('NUGET_PACKAGES', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['LIBS'] + '\nuget')) 
+{ [Environment]::SetEnvironmentVariable('NUGET_PACKAGES', $local:RemoteDriveRootFolders['LIBS'] + '\nuget', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('NUGET_HTTP_CACHE_PATH', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['CACH'] + '\nuget\http') )
+{ [Environment]::SetEnvironmentVariable('NUGET_HTTP_CACHE_PATH', $local:RemoteDriveRootFolders['CACH'] + '\nuget\http', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('NUGET_PLUGINS_CACHE_PATH', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['CACH'] + '\nuget\plugin') )
+{ [Environment]::SetEnvironmentVariable('NUGET_PLUGINS_CACHE_PATH', $local:RemoteDriveRootFolders['CACH'] + '\nuget\plugin', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('NUGET_SCRATCH', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['CACH'] + '\nuget\temp') )
+{ [Environment]::SetEnvironmentVariable('NUGET_SCRATCH', $local:RemoteDriveRootFolders['CACH'] + '\nuget\temp', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('STARSHIP_CONFIG', [EnvironmentVariableTarget]::User) -notmatch [regex]::escape($local:RemoteDriveRootFolders['CONF'] + '\starship.toml') )
+{ [Environment]::SetEnvironmentVariable('STARSHIP_CONFIG', $local:RemoteDriveRootFolders['CONF'] + '\starship.toml', [EnvironmentVariableTarget]::User) }
+
+if ( [Environment]::GetEnvironmentVariable('STARSHIP_CACHE'), [EnvironmentVariableTarget]::User -notmatch [regex]::escape($local:RemoteDriveRootFolders['CACH'] + '\starship') )
+{ [Environment]::SetEnvironmentVariable('STARSHIP_CACHE', $local:RemoteDriveRootFolders['CACH'] + '\starship', [EnvironmentVariableTarget]::User) }
+
+
+function Invoke-Starship-TransientFunction {
+    &starship module character
+}
+
+Invoke-Expression (&starship init powershell)
+Enable-TransientPrompt
+
+
+# Write-Host $RemoteDriveLetter
+# Write-Host $RemoteDrive
+# Write-Host $local:Path
 Get-ChildItem .\scripts | ForEach-Object -Process { & $_ }
