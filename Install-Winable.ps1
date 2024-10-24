96010b8946f160b75649bb5457430e0d2b40e1d7
Darian Samuel Hood <hoodds@proton.me>
2024-10-24 12:59:47 -0500

    Created and started work on Install-Winable.ps1
    When ran will clone the winable branch and move all files in the
    correct directories. Clean up will be its own file.

---
+++ Install-Winable.ps1
@@ -0,0 +1,10 @@
+#TODO: Automaticly install and setup the Winable branch. AKA Portable for Windows
+<# File System Layout ╠ ║ ╔ ╚
+.\
+╠ .cache | cache files
+╠ .conf  | config files
+╠ .libs  | library files
+╠ sbin   | program lanagues
+╠ srv    | programs
+╚ proj   | projects
+#>
